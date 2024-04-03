b1fecb165984273a159e095596ed002fdfd1b250
Francislene Menezes de Souza <142364644+FranSou23@users.noreply.github.com>
2024-04-02 21:53:09 -0300

    aula 02/04/2024

---
+++ contato.php
@@ -0,0 +1,38 @@
+<?php include("cabecalho.php") ?>
+<?php include("menu.php") ?>
+<main class="bg-light row pe-3 ps-3 mb-5">
+            <div class="row">
+                <h2 class="mt-4 h1">Entre em contato</h2>
+                <hr>
+            </div>   
+            <div class="row">
+                <div class="col">
+                    <div class="col-12">
+                        <label for="exampleFormControlInput1" class="form-label"></label>
+                        <input type="nome" class="form-control" id="exampleFormControlInput1" placeholder="Nome">
+                    </div>
+                    <div class="col-12">
+                        <label for="exampleFormControlInput1" class="form-label"></label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="E-mail">
+                    </div>
+                    <div class="col-12">
+                        <label for="exampleFormControlInput1" class="form-label"></label>
+                        <input type="assunto" class="form-control" id="exampleFormControlInput1" placeholder="Assunto">
+                    </div>
+                    <div class="col-12">
+                        <label for="exampleFormControlTextarea1" class="form-label"></label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="4" placeholder="Mensagem"></textarea>
+                        <br>
+                    <div class="col-12 text-center">
+                        <button type="button" class="btn btn-outline-secondary">Enviar</button>
+                    </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14719.717728122687!2d-47.30216705!3d-22.73086415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1712020905156!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+            
+</main>
+
+<?php include("rodape.php") ?>
